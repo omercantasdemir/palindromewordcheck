39f11a1bb5a9c64aa680987af7b959d34ade6f2f
Ömercan Taşdemir <omercantasdemir@hotmail.com>
2022-01-26 00:08:37 +0300

    Genel hatlarıyla projeyi geliştirdim ve palindrom ifade oluşturmak için bir sınıf (Creator.cs) ve metod oluşturdum.

---
+++ palindrome/Creator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    public class Creator
+    {
+        //Palindrom kelime üretme fonksiyonu. A-Z aralığında (küçük harfler dahil değil) harflerden oluşan anlamsız
+        //palindrom ifadeler oluşturur. Belirtilen uzunlukta oluşturur ve bu ifadeleri döndürür.
+        public string palindromeCreator(int uzunluk)
+        {
+            //"uzunluk" büyüklüğünde karakter dizisi oluşturuluyor.
+            char[] temp = new char[uzunluk];
+            Random random = new Random();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                //Rastgele sayılar harflere çevriliyor ve karakter dizisine yerleştiriliyor.
+                char harf = (char)random.Next(65, 90);
+                temp[i] = harf;
+                temp[uzunluk - i - 1] = harf;
+            }
+            //Döndürülecek string değeri karakter dizisinden türetiliyor.
+            string ifade = new string(temp);
+            return ifade;
+        }
+    }
+}
--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -1,45 +1,68 @@
 ﻿using System;
-namespace palindrome_words
+namespace palindrome
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Program program = new Program();
-
-            if (program.Check("ABCDEFEDCBAA"))
+            string ifade = "";
+            do
             {
-                Console.WriteLine("Kelime Bir Palindrom.");
-            }
-            else
-            {
-                Console.WriteLine("Kelime Bir Palindrom Değildir.");
-            }
-        }
-        public bool palindromeCheck(string phrase)
-        {
-            char[] chars = phrase.ToCharArray();
-            for (int i = 0; i < phrase.Length; i++)
-            {
-                if (phrase[i] != phrase[phrase.Length - i - 1])
+                Console.Clear();
+                //İfadenin kullanıcıdan tarafından girilme veya sistem tarafından oluşturulma durumu denetleniyor.
+                Console.WriteLine("İfadeyi siz mi girersiniz biz mi oluşturalım?\n1) Ben girerim\n2) Siz oluşturun");
+                switch (Convert.ToInt16(Console.ReadLine()))
                 {
-                    return false;
+                    case 1:
+                        //İfade kullanıcı tarafından girilecekse bu ifade burada alınıyor.
+                        Console.Write("Lütfen denemek istediğiniz ifadeyi girin: ");
+                        ifade = Console.ReadLine();
+                        break;
+                    case 2:
+                        //İfade sistem tarafından oluşturulacaksa burada uzunluğu alınıp işleniyor.
+                        Creator creator = new Creator();
+                        Console.Write("Lütfen oluşturmak istediğiniz ifadenin uzunluğunu giriniz: ");
+                        ifade = creator.palindromeCreator(Convert.ToInt16(Console.ReadLine()));
+                        break;
                 }
-            }
-            return true;
+                //İfade kontrol ediliyor ve sonuca göre ekrana çıktı yapılıyor.
+                if (palindromeCheck(ifade))
+                    Console.WriteLine("Kelime Bir Palindrom.");
+                else
+                    Console.WriteLine("Kelime Bir Palindrom Değildir.");
+                //Devam edilme durumu kontrol ediliyor.
+            } while (cevap());
         }
-        bool Check(string word)
+        //Kelime kontrol işlemi yapılıyor.
+        public static bool palindromeCheck(string phrase)
         {
-
-            if (string.IsNullOrEmpty(word))
+            //İfadenin boş olma durumu kontrol ediliyor.
+            if (!string.IsNullOrEmpty(phrase))
             {
-                return false;
+                //İfadenin ekrana yazdırılması. (Sadece görsellik için.)
+                Console.WriteLine(phrase);
+                //İfade karakter dizisine dönüştürülüyor ve indexlerdeki gerekli kontroller yapılıyor.
+                char[] chars = phrase.ToCharArray();
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (phrase[i] != phrase[phrase.Length - i - 1])
+                    {
+                        //Uygunsuzluk durumu yakalanırsa yanlış döndüren kısım.
+                        return false;
+                    }
+                }
+                //Bütün karakterler kurala uyuyorsa doğru döndüren kısım.
+                return true;
             }
-            else
-            {
-                return (palindromeCheck(word));
-            }
+            //İfade boşsa yanlış döndüren kısım.
+            return false;
+        }
+        public static bool cevap()
+        {
+            Console.WriteLine("Devam etmek ister misiniz? e/h");
+            if (Console.ReadLine() == "e")
+                return true;
+            return false;
         }
     }
 }
